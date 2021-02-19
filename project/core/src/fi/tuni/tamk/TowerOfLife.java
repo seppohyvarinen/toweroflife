d00a137361a0b693d80a9fbb4b2bda49e42f96b1
seppohyvarinen <72986742+seppohyvarinen@users.noreply.github.com>
2021-02-19 10:31:09 +0200

    Some initial imports.

--- project/core/src/fi/tuni/tamk/TowerOfLife.java
+++ project/core/src/fi/tuni/tamk/TowerOfLife.java
@@ -3,31 +3,38 @@ package fi.tuni.tamk;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.World;
 
 public class TowerOfLife extends ApplicationAdapter {
+	private static final float WORLD_WIDTH = 9f;
+	private static final float WORLD_HEIGHT =16f ;
 	SpriteBatch batch;
-	Texture img;
+	private OrthographicCamera camera;
+	private World world;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false,WORLD_WIDTH,WORLD_HEIGHT);
+		world = new World(new Vector2(0, -9.8f), true);
+
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+
 		batch.end();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
-		img.dispose();
+
 	}
 }
