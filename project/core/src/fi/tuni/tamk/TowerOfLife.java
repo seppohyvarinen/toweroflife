1b590a73307f3cf482f8f1ecd951c7dce93f5481
Artem <artem.tolpa@gmail.com>
2021-02-19 13:46:56 +0200

    Create body.

--- project/core/src/fi/tuni/tamk/TowerOfLife.java
+++ project/core/src/fi/tuni/tamk/TowerOfLife.java
@@ -7,6 +7,10 @@ import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.math.Vector2;
+import com.badlogic.gdx.physics.box2d.Body;
+import com.badlogic.gdx.physics.box2d.BodyDef;
+import com.badlogic.gdx.physics.box2d.CircleShape;
+import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.World;
 
 public class TowerOfLife extends ApplicationAdapter {
@@ -15,13 +19,15 @@ public class TowerOfLife extends ApplicationAdapter {
 	SpriteBatch batch;
 	private OrthographicCamera camera;
 	private World world;
-	
+	private Body body;
+
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
 		camera = new OrthographicCamera();
 		camera.setToOrtho(false,WORLD_WIDTH,WORLD_HEIGHT);
 		world = new World(new Vector2(0, -9.8f), true);
+		body = createBody(WORLD_WIDTH / 2, WORLD_HEIGHT / 2, 0.5f);
 
 	}
 
@@ -36,5 +42,47 @@ public class TowerOfLife extends ApplicationAdapter {
 	public void dispose () {
 		batch.dispose();
 
+	}
+
+	private Body createBody(float x, float y, float radius) {
+		Body playerBody = world.createBody(getDefinitionOfBody(x, y));
+		playerBody.createFixture(getFixtureDefinition(radius));
+		return playerBody;
+	}
+
+	private BodyDef getDefinitionOfBody(float x, float y) {
+		// Body Definition
+		BodyDef myBodyDef = new BodyDef();
+
+		// It's a body that moves
+		myBodyDef.type = BodyDef.BodyType.DynamicBody;
+
+		// Initial position is centered up
+		// This position is the CENTER of the shape!
+		myBodyDef.position.set(x, y);
+
+		return myBodyDef;
+	}
+
+	private FixtureDef getFixtureDefinition(float radius) {
+		FixtureDef playerFixtureDef = new FixtureDef();
+
+		// Mass per square meter (kg^m2)
+		playerFixtureDef.density = 1;
+
+		// How bouncy object? Very bouncy [0,1]
+		playerFixtureDef.restitution = 0.6f;
+
+		// How slipper object? [0,1]
+		playerFixtureDef.friction = 0.5f;
+
+		// Create circle shape.
+		CircleShape circleshape = new CircleShape();
+		circleshape.setRadius(radius);
+
+		// Add the shape to the fixture
+		playerFixtureDef.shape = circleshape;
+
+		return playerFixtureDef;
 	}
 }
