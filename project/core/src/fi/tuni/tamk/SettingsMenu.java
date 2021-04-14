adc59591b50a1f36176560e7864dcfea15353718
Artem <artem.tolpa@gmail.com>
2021-04-14 12:29:53 +0300

    some screen fix

--- project/core/src/fi/tuni/tamk/SettingsMenu.java
+++ project/core/src/fi/tuni/tamk/SettingsMenu.java
@@ -133,8 +133,6 @@ public class SettingsMenu implements Screen {
 
     @Override
     public void render(float delta) {
-        Gdx.gl.glClearColor(0, 0, 1, 0.5f);
-        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
         stage.act();
         stage.getBatch().begin();
         stage.getBatch().draw(menuBg, 0, -6f, menuBg.getWidth() * (9 / 15f), menuBg.getHeight() * (9 / 15f));
